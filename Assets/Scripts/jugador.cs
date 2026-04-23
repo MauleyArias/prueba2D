ba189828fade03f0cdba2549be7288fed51b88f1
shirley <shirley.arias.mo@gmail.com>
2026-04-23 14:05:27 -0400

    avance audio

--- Assets/Scripts/jugador.cs
+++ Assets/Scripts/jugador.cs
@@ -17,6 +17,12 @@ public class jugador : MonoBehaviour
 
     private int cantAbejas = 0;
     public TMP_Text textoAbejas;
+    private bool enRetroceso = false;
+   
+    public AudioSource audioSource;
+    public AudioClip audioPuerquito;
+    public AudioClip audioCaracol;
+    public AudioClip audioAbeja;
 
     void Start()
     {
@@ -27,9 +33,12 @@ public class jugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!enRetroceso) {
         movimiento = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(movimiento * velocidad, rb.linearVelocity.y);
          if (movimiento!=0) transform.localScale = new Vector3(Mathf.Sign(movimiento),1,1);
+        }
+
          if (Input.GetButtonDown("Jump") && esPiso) 
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, alturaSalto);
         animator.SetFloat("Velocidad", Mathf.Abs(movimiento));
@@ -43,13 +52,34 @@ public class jugador : MonoBehaviour
     {
         if (collision.transform.CompareTag("abejita"))
         {
+            audioSource.PlayOneShot(audioAbeja);
             Destroy(collision.gameObject);
             cantAbejas++;
             textoAbejas.text = "" + cantAbejas;
         }
         if(collision.transform.CompareTag("puerquito"))
         {
+            audioSource.PlayOneShot(audioPuerquito);
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+        if (collision.transform.CompareTag("Caracol"))
+        {
+            audioSource.PlayOneShot(audioCaracol);
+            enRetroceso = true;
+            Vector2 arrastre = (rb.position - 
+(Vector2)collision.transform.position).normalized * 3;
+            rb.linearVelocity = Vector2.zero;
+            rb.AddForce(arrastre, ForceMode2D.Impulse);
+            Collider2D[] colliders = collision.GetComponents<Collider2D>();
+            foreach (Collider2D col in colliders)
+                col.enabled = false;
+            collision.GetComponent<Animator>().enabled = true;
+            Destroy(collision.gameObject, 0.4f);
+            Invoke(nameof(QuitarRetroceso), 0.2f);
+        }
+    }
+    void QuitarRetroceso()
+    {
+        enRetroceso = false;
     }
 }
